6290558f59cd2e08d2c82d5cf8f4265488678507
maianhtuan98 <63273336+maianhtuan98@users.noreply.github.com>
2020-12-10 00:58:09 +0700

    Update index.js

--- serve/index.js
+++ serve/index.js
@@ -2,7 +2,7 @@ const handler = require('serve-handler')
 const http = require('http')
 
 const server = http.createServer((request, response) => {
-  // You pass two more arguments for config and middleware.
+  // You pass two more arguments for config and middleware
   // More details here: https://github.com/zeit/serve-handler#options.
   return handler(request, response, { public: 'public' })
 })
