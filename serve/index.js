2fbb60d43dc4e22085a814e05c95d59a82f1a98a
levantho1998 <62795498+levantho1998@users.noreply.github.com>
2020-12-09 23:32:53 +0700

    Update index.js

--- serve/index.js
+++ serve/index.js
@@ -9,5 +9,5 @@ const server = http.createServer((request, response) => {
 
 const port = process.env.PORT || 3000
 server.listen(port, () => {
-  console.log('Running at http://localhost:' + port)
+  console.log('Server running at http://localhost:' + port)
 })
